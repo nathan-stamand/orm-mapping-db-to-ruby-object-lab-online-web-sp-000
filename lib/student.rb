653c4b077474f10d49ffe6dbca8698c516977829
Nathan St.Amand <nathansaint810@gmail.com>
2020-03-26 20:38:59 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -64,14 +64,12 @@ class Student
   def self.first_X_students_in_grade_10(num)
     sql = <<-SQL 
     SELECT * FROM students 
-    WHERE grade = 10 
-    ORDER BY students.id
-    LIMIT num
+    WHERE grade = 10
     SQL
     
     DB[:conn].execute(sql).map do |row|
       self.new_from_db(row)
-    end
+    end.num.times
     
   end
   
