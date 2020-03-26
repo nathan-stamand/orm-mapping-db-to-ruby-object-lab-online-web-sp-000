45de49c2fac9fb5dc6a552cf672c6a52249723ad
Nathan St.Amand <nathansaint810@gmail.com>
2020-03-26 20:31:53 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -58,7 +58,21 @@ class Student
     DB[:conn].execute(sql).map do |row|
       self.new_from_db(row)
     end
-  end 
+    
+  end
+  
+  def self.first_X_students_in_grade_10 
+    sql = <<-SQL 
+    SELECT * FROM students 
+    WHERE grade = 10 
+    LIMIT ? 
+    SQL
+    
+    DB[:conn].execute(sql, num).map do |row|
+      self.new_from_db(row)
+    end
+    
+  end
   
   def save
     sql = <<-SQL
