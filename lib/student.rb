023887167a8a5bf77d1ab13302413e3e3fd9eba2
Nathan St.Amand <nathansaint810@gmail.com>
2020-03-26 20:37:30 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -62,15 +62,7 @@ class Student
   end
   
   def self.first_X_students_in_grade_10 
-    sql = <<-SQL 
-    SELECT * FROM students 
-    WHERE grade = 10 
-    ORDER BY students.id
-    SQL
     
-    DB[:conn].execute(sql).map do |row|
-      self.new_from_db(row)
-    end
     
   end
   
