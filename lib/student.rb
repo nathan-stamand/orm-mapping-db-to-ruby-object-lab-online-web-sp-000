7d51207364d1f8d80a9a9b7ad7c5430dbc920624
Nathan St.Amand <nathansaint810@gmail.com>
2020-03-26 20:40:36 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -65,11 +65,12 @@ class Student
     sql = <<-SQL 
     SELECT * FROM students 
     WHERE grade = 10
+    LIMIT #{num}
     SQL
     
     DB[:conn].execute(sql).map do |row|
       self.new_from_db(row)
-    end.num
+    end
     
   end
   
